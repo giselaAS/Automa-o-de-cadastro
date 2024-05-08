273ff5ccdbe557fb6004af81c3c2eb5e1119a9b6
giselaAS <g154sousa@gmail.com>
2024-05-07 21:47:34 -0300

    entrando no site do sistema de cadastro

--- codigo.py
+++ codigo.py
@@ -1,11 +1,18 @@
 import pyautogui
 import pandas as pd
 
-
+# abrir o brave
 pyautogui.PAUSE = 1
-pyautogui.press('win')
-pyautogui.write('brave')
-pyautogui.press('enter')
-
-pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
-pyautogui.press('enter')
+pyautogui.press("win")
+pyautogui.write("brave")
+pyautogui.press("enter")
+# abrir o site do sistema
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+# fazer o login no sistema
+pyautogui.press("tab")
+pyautogui.write("gisasousa@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("sua senha")
+pyautogui.press("tab")
+pyautogui.press("enter")
