e6241e5e3062764b0f587a3f97d8077a2520846e
giselaAS <g154sousa@gmail.com>
2024-05-07 23:05:17 -0300

    cadastro de produtos

--- codigo.py
+++ codigo.py
@@ -1,18 +1,44 @@
 import pyautogui
 import pandas as pd
+import time
 
-# abrir o brave
 pyautogui.PAUSE = 1
 pyautogui.press("win")
-pyautogui.write("brave")
+pyautogui.write("chrome")
 pyautogui.press("enter")
-# abrir o site do sistema
+time.sleep(4)
+
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
-# fazer o login no sistema
+time.sleep(3)
+
 pyautogui.press("tab")
 pyautogui.write("gisasousa@gmail.com")
 pyautogui.press("tab")
 pyautogui.write("sua senha")
 pyautogui.press("tab")
 pyautogui.press("enter")
+time.sleep(3)
+
+
+tabela = pd.read_csv("produtos.csv")
+# print(tabela)
+
+for linha in tabela.index:
+    pyautogui.click(x=680, y=226)
+    pyautogui.write(str(tabela.loc[linha, "codigo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    if not pd.isna("obs"):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
